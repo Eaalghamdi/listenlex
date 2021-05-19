adc09123b43662986f075c994ad8842db257315f
Emad <eaalghamdi@kau.edu.sa>
2021-05-19 17:56:18 +1000

    update

--- client/src/views/Experiment.vue
+++ client/src/views/Experiment.vue
@@ -251,7 +251,7 @@ export default {
     },
     async submitData() {
       await axios
-        .post("/api/activities/", this.trakingData[0])
+        .post("api/activities/", this.trakingData[0])
         .then(
           function() {
             this.trakingData = [];
